3da21f418b080e5658175756b61a72588194320e
BeomChu <izekfzha@gmail.com>
2021-12-07 15:34:09 +0900

    first commit
    빌드

---
+++ src/main/java/com/super5th/ezen/Super5thApplication.java
@@ -0,0 +1,13 @@
+package com.super5th.ezen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Super5thApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Super5thApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/super5th/ezen/Super5thApplicationTests.java
@@ -0,0 +1,13 @@
+package com.super5th.ezen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Super5thApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
